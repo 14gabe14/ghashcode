3a61564f204cd477bdd1e2e4e7202dea24157ebc
Gabriel Peralta <30735203+14gaby14@users.noreply.github.com>
2020-02-20 15:26:38 -0500

    Update ghc.c

--- ghc.c
+++ ghc.c
@@ -5,23 +5,21 @@ int main(void){
     int books; //number of books
     int num_libraries;
     int days; //total amount of available days
+    
     int books_score[N];
     int num_books_per_library[N];
     int libraries[1000][N];
+    int Avg_Lib_Score[N]; //sum of book scores by total number of books
+
+    int max_scanned[N]; //maximum books that can be scanned per day
+    int signup_days[N]; //days required to sign up  all books
+
 
     FILE *fptr;
     fptr = fopen("a_example.txt","r");
     fscanf(fptr, "%d %d %d", &books, &num_libraries, &days);
     printf("%d", books);
     fclose(fptr);
-
-    int max_scanned[N]; //maximum books that can be scanned per day
-    int signup_days[N]; //days required to sign up  all books
-
-    int books_score[N];
-    int num_books_per_library[N];
-    int libraries[N][N];
-    int Avg_Lib_Score[N]; //sum of book scores by total number of books
 
     for(int i=0;i<num_libraries;i++){
         for(int j=0;j<num_books_per_library[i];i++){
