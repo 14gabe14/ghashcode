b3d289c21337093d6403b3fabcf2267b301fbfd0
Gabriel Peralta <30735203+14gaby14@users.noreply.github.com>
2020-02-20 16:21:25 -0500

    Update ghc.c

--- ghc.c
+++ ghc.c
@@ -21,16 +21,15 @@ int main(void){
     fscanf(fptr, "%d %d %d", &books, &num_libraries, &days);
     for(int i = 0; i < books; i++){
         fscanf(fptr, "%d ", &books_score[i]);
-        printf("%d", books_score[i]);
     }
     for(int i = 0; i < num_libraries; i++){
         fscanf(fptr, "%d %d %d", &num_books_per_library[i], &signup_days[i], &max_scanned[i]);
         for(int j = 0; j < num_books_per_library[i]; j++){
-            fscanf(fptr, "%d ", libraries[i][j]);
+            fscanf(fptr, "%d ", &libraries[i][j]);
         }
     }
     fclose(fptr);
-    
+    /*
     for(int i=0;i<num_libraries;i++){
         for(int j=0;j<num_books_per_library[i];i++){
             Avg_Lib_Score[i]=books_score[i]/num_books_per_library[i];
@@ -40,6 +39,6 @@ int main(void){
             }
         }
     }
-    
+    */
 }
 
