6e4d7e9c4fb0c4256c1629b8490339da0d1dd1ec
Gabriel Peralta <30735203+14gaby14@users.noreply.github.com>
2020-02-20 15:21:16 -0500

    Update ghc.c

--- ghc.c
+++ ghc.c
@@ -5,9 +5,13 @@ int main(void){
     int books; //number of books
     int num_libraries;
     int days; //total amount of available days
-
     int books_score[N];
     int num_books_per_library[N];
-    int libraries[N][N];
+    int libraries[1000][N];
 
+    FILE *fptr;
+    fptr = fopen("a_example.txt","r");
+    fscanf(fptr, "%d %d %d", &books, &num_libraries, &days);
+    printf("%d", books);
+    fclose(fptr);
 }
