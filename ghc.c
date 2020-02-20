4044546d420ba6fb82654862a4acd46753576771
Gabriel Peralta <30735203+14gaby14@users.noreply.github.com>
2020-02-20 14:06:05 -0500

    Update ghc.c

--- ghc.c
+++ ghc.c
@@ -2,9 +2,12 @@
 #define N 100000
 
 int main(void){
-    int books;
-    int libraries;
-    int days;
+    int books; //number of books
+    int num_libraries;
+    int days; //total amount of available days
 
     int books_score[N];
+    int num_books_per_library[N];
+    int libraries[N][N];
+
 }
